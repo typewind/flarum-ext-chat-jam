4c63aa695223e51d5688c8154f96e562d8cb1bea
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-12-05 18:27:21 -0500

    Use new admin interface

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,48 +1,64 @@
 import { extend } from 'flarum/extend';
 import app from 'flarum/app';
 import PermissionGrid from 'flarum/components/PermissionGrid';
-import ChatSettingsModal from './components/ChatSettingsModal';
 
 app.initializers.add('pushedx-chat', (app) => {
-    app.extensionSettings['xelson-chat'] = () => app.modal.show(ChatSettingsModal);
-
-    extend(PermissionGrid.prototype, 'viewItems', (items) => {
-        items.add('pushedx-chat.permissions.enabled', {
+    app.extensionData
+        .for('pushedx-chat')
+        .registerSetting({
+            setting: 'pushedx-chat.settings.charlimit',
+            label: app.translator.trans('pushedx-chat.admin.settings.charlimit'),
+            type: 'number'
+        })
+        .registerSetting({
+            setting: 'pushedx-chat.settings.floodgate.number',
+            label: app.translator.trans('pushedx-chat.admin.settings.floodgate.number'),
+            type: 'number'
+        })
+        .registerSetting({
+            setting: 'pushedx-chat.settings.floodgate.time',
+            label: app.translator.trans('pushedx-chat.admin.settings.floodgate.time'),
+            type: 'text'
+        })
+        .registerSetting({
+            setting: 'pushedx-chat.settings.display.minimize',
+            label: 'pushedx-chat.admin.settings.display.minimize',
+            type: 'switch'
+        })
+        .registerSetting({
+            setting: 'pushedx-chat.settings.display.censor',
+            label: app.translator.trans('pushedx-chat.admin.settings.display.censor'),
+            type: 'switch'
+        })
+        .registerPermission({
             icon: 'fas fa-eye',
             label: app.translator.trans('pushedx-chat.admin.permissions.enabled'),
             permission: 'pushedx-chat.permissions.enabled',
             allowGuest: true,
-        });
-    });
-
-    extend(PermissionGrid.prototype, 'startItems', (items) => {
-        items.add('pushedx-chat.permissions.create', {
+        }, 'view')
+        .registerPermission({
             icon: 'fas fa-comment-medical',
             label: app.translator.trans('pushedx-chat.admin.permissions.create'),
-            permission: 'pushedx-chat.permissions.create',
-        });
-        items.add('pushedx-chat.permissions.create.channel', {
+            permission: 'pushedx-chat.permissions.create'
+        }, 'start')
+        .registerPermission({
             icon: 'fas fa-comment-medical',
             label: app.translator.trans('pushedx-chat.admin.permissions.create.channel'),
             permission: 'pushedx-chat.permissions.create.channel',
-        });
-    });
-
-    extend(PermissionGrid.prototype, 'replyItems', (items) => {
-        items.add('pushedx-chat.permissions.chat', {
+        }, 'start')
+        .registerPermission({
             icon: 'fas fa-comments',
             label: app.translator.trans('pushedx-chat.admin.permissions.post'),
             permission: 'pushedx-chat.permissions.chat',
-        });
-        items.add('pushedx-chat.permissions.edit', {
+        }, 'reply')
+        .registerPermission({
             icon: 'fas fa-pencil-alt',
             label: app.translator.trans('pushedx-chat.admin.permissions.edit'),
             permission: 'pushedx-chat.permissions.edit',
-        });
-        items.add('pushedx-chat.permissions.delete', {
+        }, 'reply')
+        .registerPermission({
             icon: 'far fa-trash-alt',
             label: app.translator.trans('pushedx-chat.admin.permissions.delete'),
             permission: 'pushedx-chat.permissions.delete',
-        });
-    });
+        }, 'reply')
 });
